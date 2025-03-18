3bf2457d2cc74265b1c1fcc299033fc4fb923b92
koudoudou <150090844+koudoudou@users.noreply.github.com>
2025-03-18 22:19:50 +0200

    AddPost
    nepabaigta

--- app/src/main/java/com/example/augaluratas/AppActivity.java
+++ app/src/main/java/com/example/augaluratas/AppActivity.java
@@ -9,12 +9,15 @@ public class AppActivity extends Application {
     static PlantsDatabase db;
     static UsersDatabase db2;
 
+    static PostsDatabase db3;
+
     @Override
     public void onCreate(){
         super.onCreate();
         db = Room.databaseBuilder(getApplicationContext(),PlantsDatabase.class,"my_app_db").allowMainThreadQueries().fallbackToDestructiveMigration().build();
         //Log.d("DB_CHECK", "Duomenų bazė sukurta!");
         db2 = Room.databaseBuilder(getApplicationContext(),UsersDatabase.class,"users_db").allowMainThreadQueries().build();
+        db3 = Room.databaseBuilder(getApplicationContext(),PostsDatabase.class,"posts_db").allowMainThreadQueries().build();
     }
 
     public static PlantsDatabase getDatabase()
@@ -25,5 +28,9 @@ public class AppActivity extends Application {
     public static UsersDatabase getUsersDatabase()
     {
         return db2;
+    }
+    public static PostsDatabase getPostsDatabase()
+    {
+        return db3;
     }
 }
