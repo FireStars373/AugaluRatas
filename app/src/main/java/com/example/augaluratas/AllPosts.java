f4d365923a5bb27c0d842e240ea6f43eb50a49ab
koudoudou <150090844+koudoudou@users.noreply.github.com>
2025-03-19 22:34:48 +0200

    ayo

--- app/src/main/java/com/example/augaluratas/AllPosts.java
+++ app/src/main/java/com/example/augaluratas/AllPosts.java
@@ -11,20 +11,31 @@ import android.os.AsyncTask;
 import android.widget.ImageButton;
 import android.widget.Toast;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class AllPosts extends AppCompatActivity {
     private PostsDatabase db;
-
+    private  UsersDatabase db2;
+    private ExecutorService executorService = Executors.newSingleThreadExecutor();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_posts);
 
-        db = PostsDatabase.getDatabase(this);
-        db.postsDAO().insert(new Posts(1,"Bananmedis", "fainas medis", new byte[1], 2));
+        //db = PostsDatabase.getDatabase(this);
+        //db.postsDAO().insert(new Posts(1,"Bananmedis", "fainas medis", new byte[1], 2));
         // Gauname visus įrašus foninėje gijoje
-        new GetPostsTask().execute();
 
+        /*executorService.execute(() -> {
+            db2 = UsersDatabase.getDatabase(this);
+            Users user = db2.usersDAO().getUserByUsername("MAR");
+
+
+            db = PostsDatabase.getDatabase(this);
+            db.postsDAO().insert(new Posts(user.getId(),"Bananmedis", "fainas medis", new byte[1], 2));
+            new GetPostsTask().execute();
+        });*/
         ImageButton sidebar = findViewById(R.id.sidebar_from_all_posts);
         ImageButton shopping_cart = findViewById(R.id.shopping_cart);
 
--- app/src/main/java/com/example/augaluratas/UsersDatabase.java
+++ app/src/main/java/com/example/augaluratas/UsersDatabase.java
@@ -6,5 +6,17 @@ import androidx.room.RoomDatabase;
 @Database(entities = {Users.class}, version = 1, exportSchema = false)
 public abstract class UsersDatabase extends RoomDatabase {
     public abstract UsersDAO usersDAO();
-
+    private static UsersDatabase INSTANCE;
+    public static UsersDatabase getDatabase(Context context) {
+        if (INSTANCE == null) {
+            synchronized (PostsDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                    UsersDatabase.class, "users_database")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
