d016e387ec1279971201df4d9012312957cc438e
GuodaBal <az.guodabaltutyte@gmail.com>
2025-03-20 11:58:52 +0200

    notification_fix

--- app/src/main/java/com/example/augaluratas/Help.java
+++ app/src/main/java/com/example/augaluratas/Help.java
@@ -1,6 +1,9 @@
 package com.example.augaluratas;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -25,7 +28,10 @@ public class Help extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_help);
-        NotificationHandler.createNotificationChannel(Help.this);
+
+        // Create notification channel FIRST
+        createNotificationChannel();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.contstraint_layout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -35,17 +41,33 @@ public class Help extends AppCompatActivity {
         ImageButton return_button = findViewById(R.id.return_from_help);
         return_button.setOnClickListener(v -> finish());
 
-        //Notification handling logic
         requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {});
+
         Button sendButton = findViewById(R.id.button45);
         sendButton.setOnClickListener(v -> {
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
                 if (checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
                     requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS);
                 } else {
-                    NotificationHandler.sendNotification(Help.this, "", "Jūsų skelbimas buvo įsimintas!", Help.class, 1);
+                    NotificationHandler.sendNotification(Help.this, "app_channel_id", "Jūsų skelbimas buvo įsimintas!", Help.class, 1);
                 }
             }
         });
+    }
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is not in the Support Library.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "kanalo pavadinimas";
+            String description = "kanalo aprasymas";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("app_channel_id", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system. You can't change the importance
+            // or other notification behaviors after this.
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
     }
 }
